addd93e615cc8b71c338f500a6f5307b72fb30c4
Carlos Hernandez <chernandez@teknei.com>
2023-04-25 13:12:04 -0600

    Add Tab

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginViewController = LoginViewController()
     let onboardingController = OnboardingContainerViewController()
     let dummyController = DummyViewController()
+    let mainViewController = MainViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     
@@ -24,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingController.delegate = self
         dummyController.delegate = self
-        window?.rootViewController = loginViewController
+        window?.rootViewController = mainViewController
         
         return true
     }
